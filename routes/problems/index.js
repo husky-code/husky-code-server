07727e193a484ae8b39b1233efa1391f835d28a2
E70M <eitanmyron@outlook.com>
2019-09-12 09:36:27 -0700

    Routes
    Select problem by course

--- routes/problems/index.js
+++ routes/problems/index.js
@@ -11,7 +11,33 @@ router.get('/', (req, res) => { // For dev purposes only
 		if (problems == null) {
 			res.json({
 				status: res.statusCode,
-				msg: 'No existing users'
+				msg: 'No existing problems'
+			});
+		} else {
+			res.json({
+				status: res.statusCode,
+				problems: problems
+			});
+		}
+	}).catch(err => {
+		res.status(500).json({
+			status: res.statusCode,
+			err: err
+		});
+	});
+});
+
+// TODO: correct req param and model field
+router.get('/:course', (req, res) => {
+	Problems.findAll({
+		where: {
+			course: req.params.course
+		}
+	}).then(problems => {
+		if (problems === null) {
+			res.json({
+				status: res.statusCode,
+				msg: 'No existing problems listed under that course'
 			});
 		} else {
 			res.json({
